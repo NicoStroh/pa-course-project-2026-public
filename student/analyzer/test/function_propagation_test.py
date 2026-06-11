8b2fc6789c0796012d8620f9c28337a2c412e438
NicoStroh <nico.strohbach@yahoo.de>
2026-06-11 18:15:19 +0200

    enhanced function vulnerabilities for all kinds of sinks

--- student/analyzer/test/function_propagation_test.py
+++ student/analyzer/test/function_propagation_test.py
@@ -1,9 +1,21 @@
 import sys
 import os
+import pickle
+import sqlite3
+
+connection = sqlite3.connect(":memory:")
 
 def foo(x):
+    os.system(x)
+    eval(x)
+    pickle.load(x)
+    connection.execute(x)
     os.open(x)
     return x
 
 a = foo(sys.argv[1])
+os.system(a)
+eval(a)
+pickle.load(a)
+connection.execute(a)
 os.open(a)
